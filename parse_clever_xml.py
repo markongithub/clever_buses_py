1ec21478e7623c3d7d70c6aa96eb374e16d5fa2e
mark on github <markongithub@users.noreply.github.com>
2022-09-13 22:50:03 +0000

    initial commit of this garbage

---
+++ parse_clever_xml.py
@@ -0,0 +1,73 @@
+import os
+import re
+import sys
+from xml.etree.ElementTree import parse
+from collections import defaultdict
+import pandas as pd
+from datetime import datetime
+import gc
+
+# No I can't use read_xml because it doesn't handle "N/A" correctly.
+def etree_to_dict(t):
+    d = {t.tag: {} if t.attrib else None}
+    children = list(t)
+    if children:
+        dd = defaultdict(list)
+        for dc in map(etree_to_dict, children):
+            for k, v in dc.items():
+                dd[k].append(v)
+        d = {t.tag: {k: v[0] if len(v) == 1 else v
+                     for k, v in dd.items()}}
+    if t.attrib:
+        d[t.tag].update(('@' + k, v)
+                        for k, v in t.attrib.items())
+    if t.text:
+        text = t.text.strip()
+        if children or t.attrib:
+            if text:
+              d[t.tag]['#text'] = text
+        else:
+            d[t.tag] = text
+    return d
+
+example_file = './buses2022-03-01T01:30:04.xml'
+relevant_keys = ['id', 'rt', 'dd', 'dn', 'lat', 'lon', 'pid', 'run', 'op', 'bid', 'fs']
+
+def time_from_filename(filename):
+  date_str = re.search('buses(.+)\.xml', filename).group(1)
+  return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S')
+
+def bus_dicts_from_file(filename):
+  doc = parse(open(filename))
+  retrieved_at = time_from_filename(filename)
+  bus_dicts = []
+  for bus in doc.findall('bus'):
+    old_dict = etree_to_dict(bus)['bus']
+    new_dict = { my_key: old_dict[my_key] for my_key in relevant_keys }
+    new_dict['retrieved_at'] = retrieved_at
+    bus_dicts.append(new_dict)
+  return bus_dicts
+
+def read_parquet_if_exists(filename):
+  if os.path.exists(filename):
+    return pd.read_parquet(filename)
+  else:
+    return pd.DataFrame()
+
+old_df = read_parquet_if_exists(sys.argv[1])
+rows = []
+for bus_file in sys.argv[2:]:
+  rows += bus_dicts_from_file(bus_file)
+new_df = pd.DataFrame(rows)
+final_df = pd.concat([old_df, new_df])
+old_df=pd.DataFrame()
+new_df=pd.DataFrame()
+gc.collect()
+print("Writing out final parquet file...")
+print(final_df.info())
+final_df.to_parquet(sys.argv[1])
+
+print("Confirming that written file is readable...")
+del[[final_df]]
+gc.collect()
+is_parquet_valid = pd.read_parquet(sys.argv[1])
---
+++ separate_trips.py
@@ -0,0 +1,42 @@
+import sys
+import pandas as pd
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+df = pd.read_parquet(input_file)
+
+current_trip = None
+started_at = None
+last_timestamp = None
+output_trips = []
+
+# for _, row in df.loc[df['id'] == '1758'].iterrows():
+for name, group in df.groupby('id'):
+  # if name != '1761':
+   # continue
+  for _, row in group.iterrows():
+    # Both op and rt seem to get reset randomly.
+    this_trip = {k: row[k] for k in ['fs', 'dd', 'pid', 'run', 'bid', 'id']}
+    this_coords = '{lat},{lon}'.format(lat=row['lat'][:7],lon=row['lon'][:7])
+    print(f'bus {row["id"]} was seen at {row["retrieved_at"]}')
+    if current_trip != this_trip and this_trip['fs'] != 'N/A':
+      if current_trip: # and current_trip['fs'] not in ['Not in Service', 'N/A']:
+        # print('{start} to {end}: {trip}'.format(trip=current_trip, start=started_at, end=last_timestamp))
+        output_trip = current_trip.copy()
+        output_trip['first_seen'] = started_at
+        output_trip['last_seen'] = last_timestamp
+        output_trip['start_coords'] = start_coords
+        output_trip['final_coords'] = last_coords
+        print(output_trip)
+        output_trips.append(output_trip)
+      current_trip = this_trip
+      start_coords = this_coords
+      started_at = row['retrieved_at']
+    last_timestamp = row['retrieved_at']
+    last_coords = this_coords
+
+print("Assembling final dataframe...")
+output_df = pd.DataFrame(output_trips)
+print("Writing final parquet file...")
+output_df.to_parquet(output_file)
+print("Success!")
