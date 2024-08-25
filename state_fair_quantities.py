5155b72a73ae0928593bb641098b0774886c3163
mark on github <markongithub@users.noreply.github.com>
2024-08-25 14:57:33 +0000

    show which buses were working when

---
+++ state_fair_quantities.py
@@ -0,0 +1,47 @@
+from enum import Enum
+
+import pytz
+import sys
+import pandas as pd
+
+HUB_HEAD_SIGN = "901 State Fair - Hub"
+DESTINY_HEAD_SIGN = "909 Destiny USA"
+LONG_BRANCH_HEAD_SIGN = "907 Long Branch Park"
+ALL_HEAD_SIGNS = [HUB_HEAD_SIGN, DESTINY_HEAD_SIGN, LONG_BRANCH_HEAD_SIGN]
+input_file = sys.argv[1]
+df = pd.read_parquet(input_file)
+
+started_at = None
+previous_buses_by_route = {}
+output_trips = []
+
+
+df['retrieved_at'].dt.tz_localize('utc')
+
+def format_time(whatever_time):
+    return whatever_time.tz_localize('utc').astimezone(pytz.timezone('US/Eastern'))
+
+for retrieved_at, group in df.groupby("retrieved_at"):
+    current_buses_by_route = {}
+    for _, row in group.iterrows():
+        head_sign = row['fs']
+        if head_sign not in ALL_HEAD_SIGNS:
+            continue
+        bus_id = row['id']
+        if head_sign in current_buses_by_route:
+            current_buses_by_route[head_sign].add(bus_id)
+        else:
+            current_buses_by_route[head_sign] = set([bus_id])
+#     print(f"current buses by route: {current_buses_by_route}")
+    if current_buses_by_route == previous_buses_by_route:
+        pass
+    else:
+        if started_at:
+            print(f"{format_time(started_at)} to {format_time(retrieved_at)}: {current_buses_by_route}")
+        started_at = retrieved_at
+        previous_buses_by_route = current_buses_by_route
+
+
+    # last_timestamp = row["retrieved_at"]
+    # last_coords = this_coords
+    # last_fair_state = current_fair_state
