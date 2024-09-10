74a46cc8f5ca680a9bbd588b221099490ed98273
mark on github <markongithub@users.noreply.github.com>
2024-09-10 00:24:36 +0000

    Add what I'm going to use for the graphing later

---
+++ calculate_wait_times.py
@@ -0,0 +1,41 @@
+import sys
+import numpy as np
+import pandas as pd
+
+
+def generate_wait_times(relevant_timestamps, route_direction, last_trip):
+    for timestamp in relevant_timestamps:
+        new_row = {
+            "route_direction": route_direction,
+            "time": timestamp,
+            # time_since_last_departure / np.timedelta64(1,'m')
+            "wait_time": (timestamp - last_trip) / np.timedelta64(1, "m"),
+        }
+        yield new_row
+
+
+df = pd.read_parquet(sys.argv[1])
+
+early_bound = "2024-08-27 12:45"
+late_bound = "2024-08-28 00:45"
+
+timestamps = pd.date_range(early_bound, late_bound, freq="1min")
+
+rows = []
+for route_direction, group in df.query(
+    "start_time >= @early_bound and start_time <= @late_bound"
+).groupby(["route_name", "direction"]):
+    print(route_direction)
+    last_trip = timestamps[0]
+    for _, row in group.iterrows():
+        next_trip = row["start_time"]
+        relevant_timestamps = timestamps[
+            (timestamps >= last_trip) & (timestamps <= next_trip)
+        ]
+        rows += generate_wait_times(relevant_timestamps, route_direction, last_trip)
+        last_trip = next_trip
+    relevant_timestamps = timestamps[(timestamps >= last_trip)]
+    rows += generate_wait_times(relevant_timestamps, route_direction, last_trip)
+
+output = pd.DataFrame(rows)
+print(output)
