7f70c9d93b241b5a522ddb8ad899fe292506949e
mark on github <markongithub@users.noreply.github.com>
2024-09-10 07:18:40 -0400

    split this into main and a function

--- calculate_wait_times.py
+++ calculate_wait_times.py
@@ -14,28 +14,38 @@ def generate_wait_times(relevant_timestamps, route_direction, last_trip):
         yield new_row
 
 
-df = pd.read_parquet(sys.argv[1])
+def generate_wait_times_df(trips_df, early_bound, late_bound):
+    timestamps = pd.date_range(early_bound, late_bound, freq="1min")
 
-early_bound = "2024-08-27 12:45"
-late_bound = "2024-08-28 00:45"
-
-timestamps = pd.date_range(early_bound, late_bound, freq="1min")
-
-rows = []
-for route_direction, group in df.query(
-    "start_time >= @early_bound and start_time <= @late_bound"
-).groupby(["route_name", "direction"]):
-    print(route_direction)
-    last_trip = timestamps[0]
-    for _, row in group.iterrows():
-        next_trip = row["start_time"]
-        relevant_timestamps = timestamps[
-            (timestamps >= last_trip) & (timestamps <= next_trip)
-        ]
+    rows = []
+    for route_direction, group in trips_df.query(
+        "start_time >= @early_bound and start_time <= @late_bound"
+    ).groupby(["route_name", "direction"]):
+        print(route_direction)
+        last_trip = timestamps[0]
+        for _, row in group.iterrows():
+            next_trip = row["start_time"]
+            relevant_timestamps = timestamps[
+                (timestamps >= last_trip) & (timestamps <= next_trip)
+            ]
+            rows += generate_wait_times(relevant_timestamps, route_direction, last_trip)
+            last_trip = next_trip
+        relevant_timestamps = timestamps[(timestamps >= last_trip)]
         rows += generate_wait_times(relevant_timestamps, route_direction, last_trip)
-        last_trip = next_trip
-    relevant_timestamps = timestamps[(timestamps >= last_trip)]
-    rows += generate_wait_times(relevant_timestamps, route_direction, last_trip)
 
-output = pd.DataFrame(rows)
-print(output)
+    output = pd.DataFrame(rows)
+    return output
+
+
+def main():
+    df = pd.read_parquet(sys.argv[1])
+
+    early_bound = "2024-08-27 12:45"
+    late_bound = "2024-08-28 00:45"
+
+    output = generate_wait_times_df(df, early_bound, late_bound)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
