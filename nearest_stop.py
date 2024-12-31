5d27abca000847e54e22c8c10045eecb5763f441
mark on github <markongithub@users.noreply.github.com>
2024-12-31 18:01:22 +0000

    hot damn this kind of works

--- nearest_stop.py
+++ nearest_stop.py
@@ -1,18 +1,109 @@
+import sys
 import pandas as pd
 from sortedcontainers import SortedList
+from math import radians, cos, sin, asin, sqrt
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    """
+    Calculate the great circle distance in kilometers between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
+
+
+TEST_COORDS = (43.0614, -76.059)
+LAT_RADIUS = 0.005
+LON_RADIUS = 0.012
+
+
+def get_stop_lat(row):
+    return row["stop_lat"]
+
+
+def get_stop_lon(row):
+    return row["stop_lon"]
+
+
+class StopIndex:
+    def __init__(self, stops_csv_path):
+        self.lat_index = SortedList([], key=get_stop_lat)
+        for _, row in pd.read_csv(sys.argv[1]).iterrows():
+            self.lat_index.add(row)
+
+    def find_stop(self, lat, lon):
+        best_distance = 99999
+        best_stop = None
+        stops_checked = 0
+        stops_checked_haversine = 0
+        for stop in self.lat_index.irange(
+            minimum={"stop_lat": lat - LAT_RADIUS},
+            maximum={"stop_lat": lat + LAT_RADIUS},
+        ):
+            stops_checked += 1
+            if abs(stop["stop_lon"] - lon) <= LON_RADIUS:
+                stops_checked_haversine += 1
+                distance = haversine(lat, lon, stop["stop_lat"], stop["stop_lon"])
+                print(f"{stop['stop_name']} is {distance} away from my goal.")
+                if best_stop is None or distance < best_distance:
+                    best_distance = distance
+                    best_stop = stop["stop_name"]
+        print(
+            f"We considered {stops_checked} stops and calculated {stops_checked_haversine} distances."
+        )
+        if best_stop:
+            print(f"Best guess for this stop: {best_stop}, {best_distance} km away.")
+        else:
+            print("We didn't find any good candidates for this stop.")
+        return best_stop
+
 
 def main():
-    df = pd.read_csv(sys.argv[1])
+    # df = pd.read_csv(sys.argv[1])
 
-    sl = SortedList(key=lambda r: r["stop_lat"])
+    stop_index = StopIndex(sys.argv[1])
+    test_lat, test_lon = TEST_COORDS
+    best_stop = stop_index.find_stop(test_lat, test_lon)
+    return
+    lat_index = SortedList([], key=get_stop_lat)
     for _, row in df.iterrows():
-	sl.add(r)
-
-    for stop in sl.irange(minimum=43.065120, maximum=43.065130):
-        print(stop["stop_name"])
+        # p rint(f"This row's key will be {get_stop_lat(row)}")
+        lat_index.add(row)
+    print(len(lat_index))
+    # print(lat_index[0])
+    # index = lat_index.bisect_left(43.065120)
+    # print(index)
+    # print(lat_index[index])
+    test_lat, test_lon = TEST_COORDS
+    best_distance = 99999
+    best_stop = None
+    stops_checked = 0
+    for stop in lat_index.irange(
+        minimum={"stop_lat": test_lat - LAT_RADIUS},
+        maximum={"stop_lat": test_lat + LAT_RADIUS},
+    ):
+        stops_checked += 1
+        distance = haversine(test_lon, test_lat, stop["stop_lon"], stop["stop_lat"])
+        print(f"{stop['stop_name']} is {distance} away from my goal.")
+        if best_stop is None or distance < best_distance:
+            best_distance = distance
+            best_stop = stop["stop_name"]
+    print(f"We checked {stops_checked} stops.")
+    if best_stop:
+        print(f"Best guess for this stop: {best_stop}, {best_distance} km away.")
+    else:
+        print("We didn't find any good candidates for this stop.")
 
 
 if __name__ == "__main__":
     main()
-
-
+    print("Apparently main completed.")
