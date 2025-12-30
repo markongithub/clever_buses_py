30eca74ad7df74c4213ad2bdd5e374c5efe263ad
mark on github <markongithub@users.noreply.github.com>
2025-12-29 19:39:55 -0500

    Treat late differently from early

--- correlate_gtfs.py
+++ correlate_gtfs.py
@@ -274,13 +274,12 @@ def correlate(buses_parquet, gtfs_dir, output_csv, date, time_window_minutes=15)
             ].copy()
             # print(f"Candidates: {candidates}")
             if not candidates.empty:
-                # arrival_dt is tz-aware UTC; compute absolute time diff
-                # TODO: stop using absolute value. Make early negative and late positive. Or the other way around.
-                candidates["dt_abs"] = (candidates["arrival_dt"] - retrieved_at).abs()
-                within = candidates.loc[candidates["dt_abs"] <= window]
+                # arrival_dt is tz-aware UTC
+                candidates["late"] = retrieved_at - candidates["arrival_dt"]
+                within = candidates.loc[candidates["late"].abs() <= window]
                 # print(f"within: {within}")
                 if not within.empty:
-                    best_index = within["dt_abs"].idxmin()
+                    best_index = within["late"].idxmin()
                     best = within.loc[best_index]
                     # Only populate if we haven't already observed this scheduled stop
                     if (
@@ -292,7 +291,7 @@ def correlate(buses_parquet, gtfs_dir, output_csv, date, time_window_minutes=15)
                         merged.at[best_index, "lat"] = lat
                         merged.at[best_index, "lon"] = lon
                         merged.at[best_index, "time_diff_s"] = int(
-                            best["dt_abs"].total_seconds()
+                            best["late"].total_seconds()
                         )
                     else:
                         recorded_bus = merged.at[best_index, "bus_id"]
